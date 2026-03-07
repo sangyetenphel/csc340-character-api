f6562825bc3de068b17f28839a21c3e688a73b56
sangyetenphel <sangyetenphel@gmail.com>
2026-03-06 20:41:25 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/csc340/characters_api/CharactersApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.characters_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340/characters_api/CharactersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.characters_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
